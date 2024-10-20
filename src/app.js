29b876546477f032b96908ae4709f7fcfca35e34
Yunus96 <mohammadyunus102@gmail.com>
2024-10-20 15:20:03 +0530

    fix bug

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -16,6 +22,8 @@ app.use(express.static("public"))
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
+
 
 //routes import
 import userRouter from './routes/user.routes.js'
@@ -28,4 +36,9 @@ app.use("/api/v1/email/", userRouter)
 app.use('/api/v1/favourite', userRouter)
 app.use("/api/v1/roc", userRouter)
 
+app.get("/", (req, res)=>{
+    app.use(express.static(path.resolve(__dirname,'frontend', "dist")))
+    res.sendFile(path.resolve(__dirname,'frontend', "dist", "index.html"))
+
+})
 export { app }
