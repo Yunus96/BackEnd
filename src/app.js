a3cc264cd106a19b8047fb339752a2219fd16b7f
Yunus96 <mohammadyunus102@gmail.com>
2024-10-20 18:35:18 +0530

    fix bug

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(cors({
-    origin: "http://scintillating-gnome-7bea21.netlify.app",
+    origin: "https://backend-1-fgxb.onrender.com",
     credentials: true
 }))
 
